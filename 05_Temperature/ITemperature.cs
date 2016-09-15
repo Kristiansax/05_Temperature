ce2ebd44088db7099712021542ed15d3f9e1c248
Kristiansax <Kristiansax@KRELLES_VISION>
2016-09-15 11:22:00 +0200

    Add project files.

---
+++ 05_Temperature/ITemperature.cs
@@ -0,0 +1,7 @@
+﻿namespace _05_Temperature
+{
+    public interface ITemperature
+    {
+
+    }
+}
---
+++ 05_Temperature/Temperature.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace _05_Temperature
+{
+    public enum Unit
+    {
+        Celcius,
+        Fahrenheit
+    }
+    public class Temperature
+    {
+        public Temperature()
+        {
+
+        }
+        public int Fahrenheit;
+        public int Celcius;
+        public Temperature(Unit Type, int Input)
+        {
+            switch(Type)
+            {
+                case Unit.Fahrenheit:
+                    Fahrenheit = Input;
+                    break;
+
+                case Unit.Celcius:
+                    Celcius = Input;
+                    break;
+            }
+        }
+
+        public static double FahrenheitToCelcius(double x)
+        {
+            return (x - 32) * 5 / 9;
+        }
+        public static double CelciusToFahrenheit(double x)
+        {
+            return x * 9 / 5 + 32;
+        }
+    }
+}
